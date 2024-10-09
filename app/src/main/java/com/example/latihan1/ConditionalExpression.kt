9e4f2f87a3cd119741570effee36e97c68493a99
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-10 01:50:22 +0700

    commit 10 : menambahkan file Conditional Expression

---
+++ app/src/main/java/com/example/latihan1/ConditionalExpression.kt
@@ -0,0 +1,5 @@
+package com.example.latihan1
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditiona Expression menggunakan if-else, when, dan try-catch
