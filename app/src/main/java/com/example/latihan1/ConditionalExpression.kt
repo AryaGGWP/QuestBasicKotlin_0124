882e555894668df1e0921085c0231a9f4f113a34
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-10 02:00:46 +0700

    commit 11 : menambahkan function contoh Conditional Conditional Expression

--- app/src/main/java/com/example/latihan1/ConditionalExpression.kt
+++ app/src/main/java/com/example/latihan1/ConditionalExpression.kt
@@ -3,3 +3,28 @@ package com.example.latihan1
 // Conditional Expression
 // Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
 // Conditiona Expression menggunakan if-else, when, dan try-catch
+fun ConditonalStatement() {
+println("=== Conditional Expression===")
+
+// If-Else
+val number = 10
+if (number > 0) {
+println("Positive Number")
+} else {
+println("Negative Number")
+}
+// When
+val day = 1
+when (day) {
+1 -> println("Sunday")
+2 -> println("Monday")
+3 -> println("Tuesday")
+4 -> println("Wednesday")
+5 -> println("Thursday")
+6 -> println("Friday")
+7 -> println("Saturday")
+else -> println("Invalid Day")
+}
+// Try-Catch
+val value = "10"
+}
