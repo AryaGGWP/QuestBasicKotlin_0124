9aa702a7ef80d7fc402c154b7ab90c8975a625ee
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-10 00:28:51 +0700

    commit 5 : menambahkan catatan mengenai List dalam file "Collections"

--- app/src/main/java/com/example/latihan1/Collections.kt
+++ app/src/main/java/com/example/latihan1/Collections.kt
@@ -1,9 +1,21 @@
 package com.example.latihan1
 
+// List adalah Kumpulan data yang disusun secara berurutan
+// List bersifat ordered, artinya data yang dimasukkan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukka dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
 fun ContohList() {
     println("=== List ===")
 // List Read-Only
 val readOnlyAbjad = listOf("A", "B", "C")
    println(readOnlyAbjad)
+
+// List Mutable
+val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+println(shape)
+
+
 
 }
