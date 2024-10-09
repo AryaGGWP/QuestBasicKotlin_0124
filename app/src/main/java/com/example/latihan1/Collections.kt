c9945ebac0b99890cb28f083dfbb8ce030bc7ef1
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-10 00:33:27 +0700

    commit 6 : menambahkan contoh-contoh List dalam file "Collections"

--- app/src/main/java/com/example/latihan1/Collections.kt
+++ app/src/main/java/com/example/latihan1/Collections.kt
@@ -16,6 +16,19 @@ val readOnlyAbjad = listOf("A", "B", "C")
 val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
 println(shape)
 
+// Menambahkan data ke dalam List Mutable
+shape.add("Circle")
+println(shape)
 
+// Menghapus data dari List Mutable
+shape.remove("Triangle")
 
+// Mengubah data di dalam List Mutable
+shape[0] = "Oval"
+println(shape)
+
+// List Read-only
+val shapesLocked: List<String> = shape
+println(shapesLocked)
 }
+
