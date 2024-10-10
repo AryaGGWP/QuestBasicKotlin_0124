0ce14b9945c99ab52caf54f4dd26a7da1ddf67d5
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-10 10:22:53 +0700

    commit 13 : menambahkan file "Functions"

---
+++ app/src/main/java/com/example/latihan1/Functions.kt
@@ -0,0 +1,15 @@
+package com.example.latihan1
+
+fun withoutParameter() {
+println("== withoutParameter ==")
+println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
