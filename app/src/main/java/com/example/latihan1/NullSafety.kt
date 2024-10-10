6eb97524fe29091b9f33936bf153ed4078f32513
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-10 12:02:21 +0700

    commit 16 : menambahkan file "NullSafety" dan contohnya

---
+++ app/src/main/java/com/example/latihan1/NullSafety.kt
@@ -0,0 +1,34 @@
+package com.example.latihan1
+
+fun nullSafety() {
+// neverNull has String type
+    var neverNull: String = "This can't be null"
+
+// Throws a compiler error
+// NeverNull = null
+
+// nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+
+// This is OK
+    nullable = null
+
+// check for null in conditions
+    if (neverNull == null) {
+        println("interferredNonNull is Null")
+    } else {
+        println("interferredNonNull")
+    }
+
+// Safe call operator
+println(neverNull.length) // 18
+println(nullable?.length) // null
+
+// notNull doesn't accept null values
+fun strLength(notNull: String): Int {
+return notNull.length
+}
+    println(strLength(neverNull))
+}
+
+
